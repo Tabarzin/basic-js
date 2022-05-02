913b362d4f677d8cc3edce13a3354da000870dfc
Rustam <tabarzin@gmail.com>
2022-05-02 23:58:05 +0300

    feat: solve transfrom array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+ if (!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  } 
+  
+  let arrCopy = Array.from(arr);
+  let result = [];
+
+  
+
+  
+  for (let i = 0; i < arrCopy.length; i++) {
+     switch (arrCopy[i]) {
+         
+       case '--discard-next':
+         if (i < arrCopy.length) {
+            i = i + 2;  
+         }
+         
+        break;
+         
+       case '--discard-prev':
+         result.pop();
+         break;
+         
+       case '--double-next':
+         if (i < arrCopy.length - 1) {
+           result.push(arrCopy[i+1]);
+         }
+     
+        break;   
+       
+       case '--double-prev':
+         if (i > 0) {
+                   
+        result.push(arrCopy[i-1]);
+           }
+         break;
+         
+       default:
+         result.push(arrCopy[i]);
+         
+     }
+    }  return result;
 }
+   
+
+
 
 module.exports = {
   transform
